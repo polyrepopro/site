6c0a8c12214a486abe80b5e22f9c87df88cafa9c
Noah <hi@noahflk.com>
2024-08-06 15:23:21 -0600

    Redo CSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
         xs: ["0.75rem", { lineHeight: "1.5" }],
