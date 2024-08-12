165d2d5e0161d7c75a2b1a4bdfacf2a07e4d34d4
Noah <hi@noahflk.com>
2024-08-12 12:14:08 +0200

    Remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), mdx(), react(), svelte()],
+  integrations: [tailwind(), alpinejs(), mdx(), svelte()],
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
 });
