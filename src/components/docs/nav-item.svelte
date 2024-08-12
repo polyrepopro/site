f9520e6126b50ae7a435ba3531c8064084fa829e
Noah <hi@noahflk.com>
2024-08-12 12:12:34 +0200

    Remove all React components

---
+++ src/components/docs/nav-item.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let href;
+</script>
+
+<li>
+  <a
+    {href}
+    class="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+  >
+    <slot></slot>
+  </a>
+</li>
